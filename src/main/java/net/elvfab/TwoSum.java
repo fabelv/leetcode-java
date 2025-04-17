cc86f442d4c212b4d8ca11bdac6e87362bdee843
Fabio Elvedi <fabio.elvedi@ost.ch>
2025-04-17 17:25:51 +0200

    twosum

---
+++ src/main/java/net/elvfab/TwoSum.java
@@ -0,0 +1,55 @@
+package net.elvfab;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    // O(n^2)
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return null;
+    }
+
+    // O(n)
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return null;
+    }
+
+    // avoid 1 if and 1 lookup
+    // O(n)
+    // still slower then twoSum2? -> JIT unable to optimize?
+    public static int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(nums[0], 0);
+
+        for (int i = 1; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return null;
+    }
+
+    // all tests passed on leetcode
+}
---
+++ src/test/java/net/elvfab/TwoSumTest.java
@@ -0,0 +1,65 @@
+package net.elvfab;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TwoSumTest {
+
+    @Test
+    void twoSumExample() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = TwoSum.twoSum(nums, target);
+
+        // assertion
+        assertArrayEquals(new int[]{0, 1}, result);
+    }
+
+    @Test
+    void twoSumExample2() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = TwoSum.twoSum2(nums, target);
+
+        // assertion
+        assertArrayEquals(new int[]{0, 1}, result);
+    }
+
+    @Test
+    void twoSumExample3() {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = TwoSum.twoSum3(nums, target);
+
+        // assertion
+        assertArrayEquals(new int[]{0, 1}, result);
+    }
+
+    @Test
+    void compareImplementationsPerformance() {
+        int[] nums = IntStream.range(0, 10_000_000).toArray();
+        int target = nums[0] + nums[nums.length - 1];
+
+        long t1 = System.nanoTime();
+        TwoSum.twoSum(nums, target);
+        long t2 = System.nanoTime();
+        System.out.println("twoSum:   " + (t2 - t1) + " ns");
+
+        long t3 = System.nanoTime();
+        TwoSum.twoSum2(nums, target);
+        long t4 = System.nanoTime();
+        System.out.println("twoSum2:  " + (t4 - t3) + " ns");
+
+        long t5 = System.nanoTime();
+        TwoSum.twoSum3(nums, target);
+        long t6 = System.nanoTime();
+        System.out.println("twoSum3:  " + (t6 - t5) + " ns");
+    }
+
+
+}
